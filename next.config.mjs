72957dace5c002d57d53ee0d1e2e6b8b58a90cda
ronitsharma03 <ronitkhajuria03@gmail.com>
2024-11-22 21:12:57 +0530

    Fixing build errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,48 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-      domains: ['img.youtube.com'],
-    },
-    reactStrictMode: false,
-    typescript: {
-      ignoreBuildErrors: true
-    }
-  }
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     images: {
+//       domains: ['img.youtube.com'],
+//     },
+//     reactStrictMode: false,
+//     typescript: {
+//       ignoreBuildErrors: true
+//     }
+//   }
   
+// export default nextConfig;
+
+
+// next.config.mjs
+import path from 'path';
+
+const nextConfig = {
+  webpack(config, { isServer }) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      };
+    }
+
+    // Add custom webpack configurations here
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve('src'),
+    };
+
+    return config;
+  },
+  reactStrictMode: false,
+  typescript: {
+    // Ignore TypeScript errors during the build process
+    ignoreBuildErrors: true,
+  },
+};
+
 export default nextConfig;
