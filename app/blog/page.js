8ef528e71942589d62a4498c4e19a0aa0981cb1e
satyam0027 <bharadwajsatyam033@gmail.com>
2025-03-18 19:34:20 +0530

    fixing errors

--- app/blog/page.js
+++ app/blog/page.js
@@ -2,7 +2,7 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { blogPosts } from '@/data/blogPosts';
-import BlogCategories from '@/components/BlogCategories';
+import BlogCategories from '../components/BlogCategories';
 
 export const dynamic = 'force-static';
 
