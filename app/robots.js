690affc28ce8e96595c42e7ca3c6d6c8d8f04dea
satyam0027 <bharadwajsatyam033@gmail.com>
2025-03-18 19:11:11 +0530

    error fixed

--- app/robots.js
+++ app/robots.js
@@ -1,10 +1,15 @@
+export const dynamic = 'force-static';
+
 export default function robots() {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: ['/api/', '/admin/'],
-    },
-    sitemap: 'https://sarveshmishra.com/sitemap.xml',
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/private/', '/admin/'],
+      },
+    ],
+    sitemap: 'https://thesarveshmishra.com/sitemap.xml',
+    host: 'https://thesarveshmishra.com',
   };
 } 
--- app/sitemap.js
+++ app/sitemap.js
@@ -1,30 +1,9 @@
-export default async function sitemap() {
-  const baseUrl = 'https://sarveshmishra.com';
+export const dynamic = 'force-static';
 
-  // Get all blog posts
-  const blogPosts = [
-    {
-      id: "jyotish-shastra-qa",
-      title: "JYOTISH SHASTRA (ASTROLOGY) Q&A",
-      date: "2024-03-07",
-    },
-    {
-      id: "rajiv-ranjan-journalism",
-      title: "RAJIV RANJAN: JOURNALISM & REALITY Q&A",
-      date: "2024-03-06",
-    }
-  ];
+export default function sitemap() {
+  const baseUrl = 'https://thesarveshmishra.com';
 
-  // Create blog post URLs
-  const blogUrls = blogPosts.map((post) => ({
-    url: `${baseUrl}/blog/${post.id}`,
-    lastModified: new Date(post.date),
-    changeFrequency: 'weekly',
-    priority: 0.8,
-  }));
-
-  // Define static routes
-  const routes = [
+  return [
     {
       url: baseUrl,
       lastModified: new Date(),
@@ -32,24 +11,28 @@ export default async function sitemap() {
       priority: 1,
     },
     {
-      url: `${baseUrl}/blog`,
-      lastModified: new Date(),
-      changeFrequency: 'daily',
-      priority: 0.9,
-    },
-    {
       url: `${baseUrl}/about`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
-      priority: 0.7,
+      priority: 0.8,
     },
     {
-      url: `${baseUrl}/contact`,
+      url: `${baseUrl}/blog`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.9,
+    },
+    {
+      url: `${baseUrl}/blog/jyotish-shastra-qa`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.7,
     },
+    {
+      url: `${baseUrl}/blog/rajiv-ranjan-journalism`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.7,
+    }
   ];
-
-  return [...routes, ...blogUrls];
 } 
